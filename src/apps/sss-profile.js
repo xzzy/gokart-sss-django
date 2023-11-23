954b8121c48c18363ac5ebd846709c788ed4153b
Jason Moore <jason.moore@dbca.wa.gov.au>
2023-11-23 16:19:08 +0800

    Openlayer fixes for drawing and SSS Tour

--- src/apps/sss-profile.js
+++ src/apps/sss-profile.js
@@ -3,18 +3,18 @@ let gokartProfile=  {
     "version":"16.11.1",
     "distributionType":"release",
     "description":"Spatial Support System v3 (Django)",
-    "repositoryBranch":"AP_working_sss",
-    "lastCommit":"2e8b0ff",
-    "commitDate":"Tue Sep 19 13:15:43 2023 +0800",
-    "commitMessage":"Merge pull request #46 from amipatel6783/AP_working_sss\\nAp working sss",
-    "commitAuthor":"jawaidm <jawaid.mushtaq@dpaw.wa.gov.au>",
+    "repositoryBranch":"main",
+    "lastCommit":"89e9cbe",
+    "commitDate":"Wed Nov 22 12:57:32 2023 +0800",
+    "commitMessage":"Add Missing Module",
+    "commitAuthor":"Jason Moore <jason.moore@dbca.wa.gov.au>",
     "build":{
-        "datetime":"2023-09-28 11:18:56 AWST(+0800)",
-        "date":"2023-09-28 AWST(+0800)",
-        "time":"11-18-56 AWST(+0800)",
+        "datetime":"2023-11-23 16:16:56 AWST(+0800)",
+        "date":"2023-11-23 AWST(+0800)",
+        "time":"16-16-56 AWST(+0800)",
         "platform":"Linux",
         "host":"sss-django-dev",
-        "vendorMD5":"Hwndsuv6B-uULdA_cBkL6Q"
+        "vendorMD5":"7KwixgOJOsrNFSL4ETvwKg"
     }
 }
 export default gokartProfile
