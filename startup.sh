7cc222d1746be91c0425de5d989772609628440a
Jason Moore <jmoore@austwa.com>
2024-07-12 10:59:04 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 eval $(grep -v '^#' /container-config/.cronenv | xargs -d "\n" -I {} echo export \"{}\" )
+cat /dev/urandom | tr -dc 'a-f0-9' | fold -w 32 | head -n 1 > /app/git_hash
 env
 whoami
 # how to create a new password.
