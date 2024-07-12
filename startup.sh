fc611f80425e0bc7a76effe6c8b005552c24091b
Jason Moore <jmoore@austwa.com>
2024-07-12 16:16:13 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -22,7 +22,7 @@ if [ $ENABLE_WEB == "True" ];
 echo "Starting Gunicorn"
 # Start the second process
 
-/app/venv/bin/gunicorn appmonitor.wsgi --bind :8080 --config /app/gunicorn.ini
+/app/venv/bin/gunicorn  sss.wsgi --bind :8080 --config /app/gunicorn.ini
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start gunicorn: $status"
