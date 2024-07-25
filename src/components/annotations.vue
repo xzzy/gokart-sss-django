a198cc81d2307d14b4558f2e18b2c0660a592ce7
Nishant Phour <phournishant0@gmail.com>
2024-07-25 13:09:04 +0800

    Polygon Select Multiple Feature working now

--- src/components/annotations.vue
+++ src/components/annotations.vue
@@ -813,11 +813,11 @@
                 }
             }()
 
-            drawInter.on("addtomap",function(ev){
+            drawInter.on("drawend",function(ev){
                 vm._overlay4Selection.setMap(vm.map.olmap)
-                if (vm._listener4Selection) {
-                    vm._features4Selection.unByKey(vm._listener4Selection)
-                }
+                // if (vm._listener4Selection) {
+                //     vm._features4Selection.unByKey(vm._listener4Selection)
+                // }
                 vm._listener4Selection = vm._features4Selection.on("add",function(ev){
                     var selectedFeatures = selectedFeatures_ || vm.selectedFeatures
                     try{
