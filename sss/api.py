f3fe30c0413f2470dc41aa8e6527697c83db7f1d
Jason Moore <jmoore@austwa.com>
2024-12-17 17:56:39 +0800

    Update api.py

--- sss/api.py
+++ sss/api.py
@@ -315,7 +315,9 @@ def gokart_js(request):
 
 def gokart_client(request):
     context = {'settings': conf.settings}
-    return render(request, 'sss/client.html', context)
+    response = render(request, 'sss/client.html', context)
+    response.headers["X-FRAME-OPTIONS"] = "ALLOWALL"
+    return response
 
 def sso_profile(request):
     data= '{"authenticated": true, "email": "test.test@dbca.wa.gov.au", "username": "test.test@dbca.wa.gov.au", "first_name": "Test", "last_name": "Test", "full_name": "Test Test", "groups": "TEST,TEST1,TEST_ADMIN_TEAM,TEST_DEV_TEAM", "logout_url": "/sso/auth_logout", "session_key": "000.000.000.000.000|AUTH2-01|000dddeeefffff|1-auth2018eeedddfffgghhhtttyuuhgg", "auth_cache_hit": "success", "Frame_Options": "DENY", "Content_Type_Options": "nosniff", "client_logon_ip": "000.000.000.000", "access_token": "eeddfffuuiiidlkdldkdkdldkllksdlkdlkkjasdlksajlkdjkhlsajkdsajdlkas", "access_token_created": "2023-07-19 10:24:54", "access_token_expireat": "2023-08-16 23:59:59", "idp": "staff"}'
