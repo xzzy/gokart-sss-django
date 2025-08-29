0a4514972b6c6f4d35e4795e34573a8f4aed03a2
Nishant Phour <phournishant0@gmail.com>
2025-08-29 16:27:41 +0800

    jspdf import fix

--- src/vendor.js
+++ src/vendor.js
@@ -51,7 +51,7 @@ import Tether from 'tether'
 // Guided tour lib
 import Shepherd from 'tether-shepherd'
 //pdf generator
-import jsPDF from 'jspdf/dist/jspdf.umd'
+import jsPDF from 'jspdf/dist/jspdf.umd.js'
 import interact from 'interact.js'
 import hash from "object-hash"
 import utils from './utils.js'
