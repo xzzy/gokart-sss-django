be23590ec77fd2b395430cee2c186f66caa473b9
Jason Moore <jason.moore@dbca.wa.gov.au>
2026-01-13 14:30:02 +0800

    Move outlookmeta url to slug /api

--- src/apps/sss-profile.js
+++ src/apps/sss-profile.js
@@ -4,16 +4,16 @@ let gokartProfile=  {
     "distributionType":"release",
     "description":"Spatial Support System v3 (Django)",
     "repositoryBranch":"main",
-    "lastCommit":"f91724e",
-    "commitDate":"Wed Dec 3 10:37:13 2025 +0800",
-    "commitMessage":"Force sss reset",
+    "lastCommit":"5709970",
+    "commitDate":"Tue Jan 13 14:19:48 2026 +0800",
+    "commitMessage":"Add spatial tile cache clean up scrip for expired cache",
     "commitAuthor":"Jason Moore <jason.moore@dbca.wa.gov.au>",
     "build":{
-        "datetime":"2026-01-07 16:04:32 AWST(+0800)",
-        "date":"2026-01-07 AWST(+0800)",
-        "time":"16-04-32 AWST(+0800)",
+        "datetime":"2026-01-13 14:28:43 AWST(+0800)",
+        "date":"2026-01-13 AWST(+0800)",
+        "time":"14-28-43 AWST(+0800)",
         "platform":"Linux",
-        "host":"gokart-sss-django-userdev-6bd69d46bf-lmw8n",
+        "host":"gokart-sss-django-userdev-6bd69d46bf-nhq2x",
         "vendorMD5":"Gg1tYmuJxpIhnCX42KxDTg"
     }
 }
