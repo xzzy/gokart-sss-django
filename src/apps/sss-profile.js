c659f07e437fb08f1bf487894b04951014f5eb1e
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2026-05-22 11:49:09 +0800

    Rebuild release bundle with jQuery 3.7.1

--- src/apps/sss-profile.js
+++ src/apps/sss-profile.js
@@ -4,17 +4,17 @@ let gokartProfile=  {
     "distributionType":"release",
     "description":"Spatial Support System v3 (Django)",
     "repositoryBranch":"working",
-    "lastCommit":"234d58e",
-    "commitDate":"Fri May 22 09:56:55 2026 +0800",
-    "commitMessage":"Rebuild release bundle\\nIncorporates the three preceding fixes:\\n- receiveMessage guard against non-JSON postMessage events\\n- iD-sprite.svg Custom Line icon path corrected to /static/ prefix\\n- checkPermission debug console.log removed",
+    "lastCommit":"a766b7d",
+    "commitDate":"Fri May 22 11:47:48 2026 +0800",
+    "commitMessage":"Remove jQuery 4.0.0 compatibility shims from vendor.js\\nNow that jQuery is 3.7.1, the three shims added to work around 4.0.0\\nAPI removals are no longer needed:\\n- $.isArray  (used by foundation-datepicker; native in 3.7.1)\\n- $.fn.sort  (used by foundation-sites; native in 3.7.1)\\n- $.isFunction  (captured by jcanvas at load time; native in 3.7.1)",
     "commitAuthor":"Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>",
     "build":{
-        "datetime":"2026-05-22 10:01:15 AWST(+0800)",
+        "datetime":"2026-05-22 11:48:25 AWST(+0800)",
         "date":"2026-05-22 AWST(+0800)",
-        "time":"10-01-15 AWST(+0800)",
+        "time":"11-48-25 AWST(+0800)",
         "platform":"Linux",
         "host":"gokart-sss-django-userdev-5d8cb9c4df-kd98m",
-        "vendorMD5":"L2uZ2OrYBkQly46tJPAixA"
+        "vendorMD5":"oCZMIVi54H_J_ZNeYEWwFQ"
     }
 }
 export default gokartProfile
